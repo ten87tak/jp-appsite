79861d91695674b23bd84285d4920e3792980f70
tenandtak <114275420+tenandtak@users.noreply.github.com>
2023-04-05 23:30:14 +0900

    Replaced the email address and the password with environment variables.

--- send_email.py
+++ send_email.py
@@ -6,10 +6,14 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "ten87tak@gmail.com"
-    password = "exfzejqqmofdkoow"
+    # "EMAIL" は環境変数です。送信に使いたいメールアドレスを入力してください。
+    username = "EMAIL"
 
-    receiver = "ten87tak@gmail.com"
+    # "PASSWORD" は環境変数です。アプリパスワードを作成して入力してください。
+    password = "PASSWORD"
+
+    # "EMAIL" は環境変数です。受信に使いたいメールアドレスを入力してください。
+    receiver = "EMAIL"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
