f2386a4755df0bda623bfdabefa496916c5adb71
tenandtak <114275420+tenandtak@users.noreply.github.com>
2023-04-05 23:09:52 +0900

    Changed some wordings in the contact-me form.

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -4,10 +4,11 @@ from send_email import send_email
 st.header("Contact Me")
 
 with st.form(key="form"):
-    email_address = st.text_input("メールアドレスをご入力ください： "
-                                  "送信ボタンを押したあと、ご入力いただいたメールアドレスに確認メールなどが自動で送信されることはありません。"
-                                  "メールアドレスは管理者宛てにメッセージの一部として送信されます。"
-                                  "メッセージを受領後、ご連絡させていただきます。")
+    email_address = st.text_input(
+        "メールアドレスをご入力ください（送信ボタンを押したあと、ご入力いただいたメール"
+        "アドレスに確認メールなどが自動で送信されることはありません。メールアドレスは"
+        "管理者宛てにメッセージの一部として送信されます）:")
+
     raw_message = st.text_area("メッセージ：")
     message = f"""\
     Subject: New email from {email_address}
@@ -21,4 +22,7 @@ with st.form(key="form"):
     if submit_button:
         send_email(message)
         st.info("メッセージが送信されました (*^^*)")
+
+    st.write("メール送信がエラーになる場合は、お使いのメールクライアントから直接  "
+             "ten87tak@gmail.com  へご連絡ください m(.  .)m")
 
