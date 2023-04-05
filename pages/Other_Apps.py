6b5a5b3981b6cadb3622cbb8c76ba0f4c263f19e
tenandtak <114275420+tenandtak@users.noreply.github.com>
2023-04-05 22:49:11 +0900

    Initial commit. Whole renewed JP Appsite!

---
+++ pages/Other_Apps.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+column_1, column_2 = st.columns(2)
+
+with column_1:
+    st.header("ジオメトリ・ゲーム")
+
+    st.write("ランダムに生成された２つの座標が作り出す長方形の中に、任意に選んだ座標の点が"
+                 "入っているかどうかを当てるコマンドラインで操作するゲームです。")
+
+    st.write("プロンプトが表示されたら、画面に従って任意の X 座標と Y 座標を入力します。"
+                 "また、面積も推測します。")
+
+    st.write("最後にウィンドウが表示され、その中にその長方形と任意に選んだ座標点を表示します。")
+
+
+with column_2:
+    st.image("images/OOP_App1.PNG")
+
+    st.write("[Source Code](https://github.com/ten87tak/OOP_App1_GeoGame)")
